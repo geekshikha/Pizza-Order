fb6375680ab4b1bc050d7e03ebb56ba8238f6171
geekshikha <sssinghshikha8@gmail.com>
2025-04-12 11:02:08 +0200

    Remove upsert from pizza table

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -5,21 +5,22 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const pizzas = [
-    { name: 'Margherita', type: 'Margherita', image: '/images/margherita.jpg' },
-    { name: 'Pepperoni', type: 'Pepperoni', image: '/images/pepperoni.jpg' },
-    { name: 'Veggie', type: 'Veggie', image: '/images/veggie.jpg' },
-  ];
-
-  for (const pizza of pizzas) {
-    await prisma.pizza.upsert({
-      where: { name: pizza.name }, // Assumes `name` is unique
-      update: {}, // No updates needed for seeding
-      create: {
-        name: pizza.name,
-        type: pizza.type,
-        image: pizza.image,
-      },
+  const existingPizzas = await prisma.pizza.count();
+  if (existingPizzas === 0) {
+    await prisma.pizza.createMany({
+      data: [
+        {
+          name: 'Margherita',
+          type: 'Margherita',
+          image: '/images/margherita.jpg',
+        },
+        {
+          name: 'Pepperoni',
+          type: 'Pepperoni',
+          image: '/images/pepperoni.jpg',
+        },
+        { name: 'Veggie', type: 'Veggie', image: '/images/veggie.jpg' },
+      ],
     });
   }
 }
